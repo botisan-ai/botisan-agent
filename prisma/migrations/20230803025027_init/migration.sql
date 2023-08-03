d380cd8ff6234c9ade2da731e8bcbc929cc87a4e
Simon Liang <simon@x-tech.io>
2023-08-02 17:44:51 -1000

    update conversation schema

--- prisma/migrations/20230803025027_init/migration.sql
+++ prisma/migrations/20230803025027_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "Conversation" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "comment" TEXT,
+    "label" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
